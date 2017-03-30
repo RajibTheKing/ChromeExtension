4a6cf0fc92a28a0be1ca8a56972626c3741d673a
RajibTheKing <aporba.das@gmail.com>
2017-03-30 15:59:54 +0600

    first commit

---
+++ WebCam/Timer_Stop.js
@@ -0,0 +1,49 @@
+//create a function to stop the time 
+function stopTime() 
+{ /* check if seconds, minutes and hours are not equal to 0 */ 
+	if ( seconds !== 0 || minutes !== 0 || hours !== 0 ) 
+	{
+		 /* display the full time before reseting the stop watch */ 
+		 var fulltime = document .getElementById( "fulltime" ); 
+		 //display the full time 
+		 fulltime.style.display = "block";
+		 var rand = Math.floor((Math.random() * 10));
+		 
+		 milliseconds+=rand;
+		 if(milliseconds<10)
+			mscns = ': 00' + milliseconds;
+		else if(milliseconds<100)
+			mscns = ': 0' + milliseconds;
+		else
+			mscns = ': ' + milliseconds;
+
+		 var time = gethours + mins + secs + mscns; 
+		 fulltime.innerHTML = 'Fulltime: ' + time; 
+		 // reset the stop watch 
+		 seconds = 0; 
+		 minutes = 0; 
+		 hours = 0; 
+		 secs = '0' + seconds; 
+		 mins = '0' + minutes + ': '; gethours = '0' + hours + ': '; 
+		 /* display the stopwatch after it's been stopped */ 
+		 var x = document.getElementById ("timer"); 
+		 var stopTime = gethours + mins + secs; x.innerHTML = stopTime; 
+		 /* display all stop watch control buttons */ 
+		 var showStart = document.getElementById ('start'); 
+		 showStart.style.display = "inline-block"; 
+		 var showStop = document.getElementById ("stop"); 
+		 showStop.style.display = "inline-block"; 
+		 /* clear the stop watch using the setTimeout( ) return value 'clearTime' as ID */ 
+		 clearTimeout( clearTime ); 
+	} // if () 
+} // stopTime() 
+/* you need to call the stopTime( ) function to terminate the stop watch */ 
+
+window.addEventListener( 'load', function () 
+{
+
+	var stop = document.getElementById ("stop"); 
+	stop.addEventListener( 'click', stopTime ); 
+
+}); // stopwatch.js end 
+
---
+++ WebCam/javascript/webcam.js
@@ -0,0 +1,24 @@
+
+var video = document.querySelector("#videoElement");
+var streamGlobal ;
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) 
+{       
+	navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+
+function handleVideo(stream) 
+{
+	streamGlobal = stream;
+	console.log("Inside handle video");
+	video.src = window.URL.createObjectURL(stream);
+	//document.getElementById('videoElement').src = window.URL.createObjectURL(stream);
+	console.log(stream.id);
+	console.log(video);
+
+}
+
+function videoError(e) {
+	// do something
+}	
